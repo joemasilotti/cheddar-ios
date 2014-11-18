3a300151d47acfab2b1a8dfced820f080a3274a8
Joe Masilotti <pair+jmasilotti@pivotallabs.com>
2014-11-18 12:06:37 -0500

    Add Integration target with sample tests

---
+++ Integration/Integration.m
@@ -0,0 +1,40 @@
+//
+//  Integration.m
+//  Integration
+//
+//  Created by pivotal on 11/18/14.
+//  Copyright (c) 2014 Nothing Magical. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Integration : XCTestCase
+
+@end
+
+@implementation Integration
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
